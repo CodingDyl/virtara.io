bbfb4c25894213e07102ae3904e4e2f2ca6fd1f0
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2025-02-08 13:03:54 +0200

    Added google analytics 4 to website

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-M95T9PRH'
+}
+
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
