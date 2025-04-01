8ae148823ed45b43af343bcecb7360c608f0b3b3
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2025-04-01 18:21:04 +0200

    updated import

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,6 @@
 export const config = {
     resend: {
-        apiKey: process.env.VITE_RESEND_API_KEY || "",
+        apiKey: import.meta.env.VITE_RESEND_API_KEY || "",
     },
     email: {
         from: "Contact Form <info@virtara.co.za>",
